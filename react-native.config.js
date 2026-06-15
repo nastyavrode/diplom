33d8e1ca36c34c119bd2f4c087916c38b7681fb4
Anastasia Efanova <n.efanova14@vk.com>
2026-06-15 14:24:11 +0700

    commit json

---
+++ react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dependencies: {
+    'expo-dev-launcher': {
+      platforms: {
+        ios: null, // отключаем автолинковку для iOS
+      },
+    },
+    'expo-dev-menu': {
+      platforms: {
+        ios: null, // отключаем автолинковку для iOS
+      },
+    },
+  },
+};
